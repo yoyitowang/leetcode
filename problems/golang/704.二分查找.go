a52d104486a44a52abddf3cb39d4efb6786186ec
yoyitowang <yoyowang0720@gmail.com>
2025-01-08 22:23:24 +0800

    add 704 & day1

---
+++ problems/golang/704.二分查找.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=golang
+ *
+ * [704] 爬楼梯
+ */
+
+// @lc code=start
+func search(nums []int, target int) int {
+    left := 0
+    right := len(nums)-1
+    for left <= right {
+        mid := (left + right) // 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return -1
+    
+}
+// @lc code=end
+
