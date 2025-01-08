92653a682f3f49ca76d798ae78cb72c1a7cae187
yoyitowang <yoyowang0720@gmail.com>
2025-01-08 22:59:29 +0800

    add 977

---
+++ problems/python/977.有序数组的平方.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=977 lang=python3
+#
+# [977] 有序数组的平方
+#
+
+# @lc code=start
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        # TC: O(n)
+        # SC: O(1)
+
+        left, right = 0, len(nums)-1
+        res = []
+        while left <= right:
+            if nums[left]**2 < nums[right]**2:
+                res.append(nums[right]**2)
+                right -= 1
+            else:
+                res.append(nums[left]**2)
+                left += 1
+        return res[::-1]
+        
+# @lc code=end
+
