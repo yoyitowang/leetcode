8555f2d4e02795cfbe5eb3ca893570ce282c0912
yoyitowang <yoyowang0720@gmail.com>
2025-01-12 19:16:14 +0800

    optimize from O(n) to O(log n)

--- problems/python/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ problems/python/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -7,27 +7,31 @@
 # @lc code=start
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # TC: O(n) not fullfill O(log n)
+        # TC: O(log n)
         # SC: O(1)
-
-        
-        left, right = 0, len(nums)-1
-        res = [-1, -1]
-        while left <= right:
-            mid = (left+right)//2
-            if nums[mid] == target:
-                for i in range(mid, -1, -1):
-                    if nums[i] == target:
-                        res[0] = i
-                for i in range(mid, len(nums)):
-                    if nums[i] == target:
-                        res[1] = i
-                break
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        return res
+        def findLeft(nums, target):
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            if left < len(nums) and nums[left] == target:
+                return left
+            return -1
+        def findRight(nums, target):
+            left, right = 0, len(nums)-1
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            if right >= 0 and nums[right] == target:
+                return right
+            return -1
+            
+        return [findLeft(nums, target), findRight(nums, target)]
 # # @lc code=end
 
