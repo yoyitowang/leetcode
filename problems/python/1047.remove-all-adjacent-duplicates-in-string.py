7086969e3e754f5b90916447a33d3dcf7847abb0
yoyitowang <yoyowang0720@gmail.com>
2025-01-17 22:29:01 +0800

    add 1047

---
+++ problems/python/1047.remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1047 lang=python3
+#
+# [1047] Remove All Adjacent Duplicates In String
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        st = []
+        for char in s:
+            if st and st[-1] == char:
+                st.pop()
+            else:
+                st.append(char)
+        return ''.join(st)     
+# @lc code=end
+
