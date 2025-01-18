16a3fb8dd0bb965df35e2c42d2bb81504fd3bed1
yoyitowang <yoyowang0720@gmail.com>
2025-01-18 21:05:19 +0800

    add complexity

--- problems/python/347.top-k-frequent-elements.py
+++ problems/python/347.top-k-frequent-elements.py
@@ -7,15 +7,17 @@
 # @lc code=start
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # TC: O(n logn)
+        # SC: O(k)
         from collections import defaultdict
         counter = defaultdict(int)
         for num in nums:
             counter[num] += 1
         prior_que = []
-        counter = sorted(counter.items(), key=lambda x:x[1], reverse=True)
+        counter = sorted(counter.items(), key=lambda x:x[1], reverse=True) # TC: O(n logn)
         res = []
         for i in range(k):
-            res.append(counter[i][0])
+            res.append(counter[i][0]) # TC: O(k)
         return res     
 # @lc code=end
 
