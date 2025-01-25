8171b8675211acda374f41ff67cf5b1a73636d66
yoyitowang <yoyowang0720@gmail.com>
2025-01-25 16:52:53 +0800

    add postorder

--- problems/python/530.minimum-absolute-difference-in-bst.py
+++ problems/python/530.minimum-absolute-difference-in-bst.py
@@ -27,6 +27,31 @@ class Solution:
         for i in range(1, len(res)):
             min_res = min(res[i]-res[i-1], min_res)
         return min_res
+    
+        # # postorder
+        # # TC: O(n)
+        # # SC: O(h->n)
+        # self.min_diff = float('inf')
+        
+        # def traversal(node):
+        #     if not node:
+        #         return float('inf'), float('-inf')
+            
+        #     # left > right > root
+        #     # find largest val of left
+        #     min_left, max_left = traversal(node.left)
+        #     # find largest val of right
+        #     min_right, max_right = traversal(node.right)
+        #     # calculate and find the diff
+        #     if max_left != float('-inf'):
+        #         self.min_diff = min(self.min_diff, node.val - max_left)
+        #     if min_right != float('inf'):
+        #         self.min_diff = min(self.min_diff, min_right - node.val)
+
+        #     return min(min_left, node.val), max(max_right, node.val)
+
+        # traversal(root)
+        # return self.min_diff
             
 # @lc code=end
 
