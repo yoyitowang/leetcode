22c13ac70b207cf442183217ada4bb8f5c545dc3
yoyitowang <yoyowang0720@gmail.com>
2025-01-25 22:38:02 +0800

    add 701 with dfs

---
+++ problems/python/701.insert-into-a-binary-search-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=701 lang=python3
+#
+# [701] Insert into a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # TC: O(h)
+        # SC: O(h)
+        if not root:
+            return TreeNode(val)
+        
+        # root > left > right
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        elif root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+
+        return root
+             
+# @lc code=end
+
