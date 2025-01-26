7dbec5412c93e36f613c795d5aceb5d43497f85f
yoyitowang <yoyowang0720@gmail.com>
2025-01-27 00:21:54 +0800

    add DP solution

--- problems/python/53.maximum-subarray.py
+++ problems/python/53.maximum-subarray.py
@@ -17,6 +17,18 @@ class Solution:
                 res = total
             if total < 0:
                 total = 0
-        return res      
+        return res
+    
+        # # DP
+        # # TC: O(n)
+        # # SC: O(n)
+        # dp = [0] * len(nums)
+        # dp[0] = nums[0]
+        # for i in range(1, len(nums)):
+        #     if dp[i-1] < 0:
+        #         dp[i] = nums[i]
+        #     else:
+        #         dp[i] = dp[i-1] + nums[i]
+        # return max(dp)      
 # @lc code=end
 
