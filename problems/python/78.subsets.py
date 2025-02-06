b36f09d32035ea1aac6235fa9d4c6bd8584589b5
yoyitowang <yoyowang0720@gmail.com>
2025-02-06 23:47:45 +0800

    add 78 w/ backtracking

---
+++ problems/python/78.subsets.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=78 lang=python3
+#
+# [78] Subsets
+#
+
+# @lc code=start
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # TC: O(2^n)
+        # SC: O(n)
+        self.ans = []
+
+        def backtracking(start, path):
+            self.ans.append(path[:])
+            if start == len(nums):
+                return
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtracking(i+1, path)
+                path.pop()
+
+        backtracking(0, [])
+        return self.ans 
+# @lc code=end
+
