6519e8a0630e581b03e00230cafb2ab07e4eadbe
yoyitowang <yoyowang0720@gmail.com>
2025-02-15 00:50:39 +0800

    add dp

---
+++ problems/python/746.min-cost-climbing-stairs.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=746 lang=python3
+#
+# [746] Min Cost Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # TC: O(n)
+        # SC: O(n)
+        n = len(cost)
+        dp = [0] * (n+1)
+
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+        return dp[-1]     
+# @lc code=end
+
