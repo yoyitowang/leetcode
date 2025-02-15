955d9c80533e58b232c9410063db5fc11327ba5b
yoyitowang <yoyowang0720@gmail.com>
2025-02-15 16:33:34 +0800

    optimize O(1)

--- problems/python/198.house-robber.py
+++ problems/python/198.house-robber.py
@@ -12,23 +12,30 @@ class Solution:
         # SC: O(n)
         # steal or not
         # dfs(i) = max(dfs(i-2)+nums[i], dfs(i-1))
-        n = len(nums)        
+        # n = len(nums)        
 
-        @cache
-        def dfs(n):
-            if n < 0:
-                return 0
+        # @cache
+        # def dfs(n):
+        #     if n < 0:
+        #         return 0
                 
-            return max(dfs(n-2) + nums[n], dfs(n-1))
+        #     return max(dfs(n-2) + nums[n], dfs(n-1))
         
-        return dfs(n-1) 
+        # return dfs(n-1) 
     
         # TC: O(n)
         # SC: O(n)
-        n = len(nums)
-        dp = [0] * (n+2)
-        for i in range(n):
-            dp[i+2] = max(dp[i+1], dp[i]+nums[i])
-        return dp[n+1]
+        # n = len(nums)
+        # dp = [0] * (n+2)
+        # for i in range(n):
+        #     dp[i+2] = max(dp[i+1], dp[i]+nums[i])
+        # return dp[n+1]
+    
+        # TC: O(n)
+        # SC: O(1)
+        f0 = f1 = 0
+        for num in nums:
+            f0, f1 = f1, max(f1, f0 + num)
+        return f1
 # @lc code=end
 
