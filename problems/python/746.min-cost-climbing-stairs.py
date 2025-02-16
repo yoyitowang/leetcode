e0a824fb6464656db1b37b37c0aa5b67156566ef
yoyitowang <yoyowang0720@gmail.com>
2025-02-16 22:56:01 +0800

    update space

--- problems/python/746.min-cost-climbing-stairs.py
+++ problems/python/746.min-cost-climbing-stairs.py
@@ -7,14 +7,24 @@
 # @lc code=start
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # # TC: O(n)
+        # # SC: O(n)
+        # n = len(cost)
+        # dp = [0] * (n+1)
+
+        # for i in range(2, n+1):
+        #     dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+        # return dp[-1] 
+
+        # dp
         # TC: O(n)
-        # SC: O(n)
+        # SC: O(1)
         n = len(cost)
-        dp = [0] * (n+1)
-
+        f0 = f1 = 0
         for i in range(2, n+1):
-            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
-
-        return dp[-1]     
+            f0, f1 = f1, min(f0 + cost[i-2], f1 + cost[i-1])
+        
+        return f1
 # @lc code=end
 
