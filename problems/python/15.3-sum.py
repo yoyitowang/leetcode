cde1a3a1ae55761335595f8bcd2f954f2a2183b8
yoyitowang <yoyowang0720@gmail.com>
2025-02-22 18:18:02 +0800

    rework

--- problems/python/15.3-sum.py
+++ problems/python/15.3-sum.py
@@ -7,30 +7,31 @@
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # TC: O(n^2 + n log n)
+        # SC: O(1)
         nums.sort()
-        res = []
-        for start in range(len(nums)):
-            if start > 0 and nums[start-1] == nums[start]:
+        ans = []
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            left, right = start+1, len(nums)-1
+            left, right = i+1, len(nums)-1
             while left < right:
-                _sum = nums[start]+nums[left]+nums[right]
-                if _sum == 0:
-                    tmp = [nums[start], nums[left], nums[right]]
-                    res.append(tmp)
+                total = num + nums[left] + nums[right]
+
+                if total == 0:
+                    ans.append([num, nums[left], nums[right]])
                     while left < right and nums[left] == nums[left+1]:
                         left += 1
-                    while left < right and nums[right] == nums[right-1]:
+                    while left < right and nums[right]==nums[right-1]:
                         right -= 1
-
-                    left += 1
                     right -= 1
-                elif _sum < 0:
                     left += 1
+                    
+                elif total > 0:
+                    right -= 1
                 else:
-                    right -= 1
+                    left += 1
 
-        return res
-                
+        return ans
 # @lc code=end
 
