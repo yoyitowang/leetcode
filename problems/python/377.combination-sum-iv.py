c2a0431fad41d50a0351c7d16a5ee77af53b5459
yoyitowang <yoyowang0720@gmail.com>
2025-02-23 00:37:42 +0800

    update O(n) dp

--- problems/python/377.combination-sum-iv.py
+++ problems/python/377.combination-sum-iv.py
@@ -9,12 +9,26 @@ class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         # TC: O(n*target)
         # SC: O(target)
-        @cache
-        def dfs(i):
-            if i == 0:
-                return 1
-            return sum(dfs(i-x) for x in nums if i >= x)
+        # @cache
+        # def dfs(i):
+        #     if i == 0:
+        #         return 1
+        #     return sum(dfs(i-x) for x in nums if i >= x)
 
-        return dfs(target)    
+        # return dfs(target)
+
+        # dp
+        # TC: O(n*target)
+        # SC: O(n)
+
+        n = len(nums)
+        f = [0] * (target+1)
+        f[0] = 1
+        
+        for i in range(target+1):
+            for num in nums:
+                if i >= num:
+                    f[i] += f[i-num]
+        return f[target]
 # @lc code=end
 
