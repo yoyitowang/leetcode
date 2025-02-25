360d4a9dfc79ab1a65e2214c59e19bb65c09e1b2
yoyitowang <yoyowang0720@gmail.com>
2025-02-26 00:14:28 +0800

    rework

--- problems/python/122.best-time-to-buy-and-sell-stock-ii.py
+++ problems/python/122.best-time-to-buy-and-sell-stock-ii.py
@@ -9,12 +9,10 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # TC: O(n)
         # SC: O(1)
-        ans = 0
-        
+        profit = 0
         for i in range(1, len(prices)):
-            if (profit := prices[i] - prices[i-1]) > 0:
-                ans += profit
-        
-        return ans      
+            profit += max(0, prices[i]-prices[i-1])
+
+        return profit  
 # @lc code=end
 
