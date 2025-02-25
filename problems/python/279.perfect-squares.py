6b05fc7736281d3dd77e784d557e6ac66349df3b
yoyitowang <yoyowang0720@gmail.com>
2025-02-25 21:19:00 +0800

    update

--- problems/python/279.perfect-squares.py
+++ problems/python/279.perfect-squares.py
@@ -7,15 +7,15 @@
 # @lc code=start
 class Solution:
     def numSquares(self, n: int) -> int:
-        # TC: O(n*size)
-        # TC: O(n)
-        nums = [i*i for i in range(1, int(n**(0.5))+1)]
-        
+        # TC: O(n*n**0.5)
+        # SC: O(n)
+        floor = int(n**(0.5))
+        nums = [i*i for i in range(1, floor+1)]
         f = [0] + [inf] * n
-        f[0] = 0
+
         for num in nums:
             for j in range(num, n+1):
-                f[j] = min(f[j], f[j-num] +1)
+                f[j] = min(f[j], f[j-num]+1)
 
         return f[n]
 # @lc code=end
