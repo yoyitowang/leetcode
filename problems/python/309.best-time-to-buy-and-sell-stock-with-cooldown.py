60fa3525752acb7762fe5949de121ffbc2bcc900
yoyitowang <yoyowang0720@gmail.com>
2025-02-27 01:05:24 +0800

    add 1-dim dp

--- problems/python/309.best-time-to-buy-and-sell-stock-with-cooldown.py
+++ problems/python/309.best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -9,16 +9,43 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
 
-        # i-th buy: i-1 can't buy
-        # i-th not buy: 
-        @cache
-        def dfs(i, hold):
-            if i < 0:
-                return -inf if hold else 0
-            if hold:
-                return max(dfs(i-1, True), dfs(i-2, False)-prices[i])
-            return max(dfs(i-1, False), dfs(i-1, True)+prices[i])
+        # # i-th buy: i-1 can't buy
+        # # i-th not buy: 
+        # @cache
+        # def dfs(i, hold):
+        #     if i < 0:
+        #         return -inf if hold else 0
+        #     if hold:
+        #         return max(dfs(i-1, True), dfs(i-2, False)-prices[i])
+        #     return max(dfs(i-1, False), dfs(i-1, True)+prices[i])
         
-        return dfs(n-1, False)    
+        # return dfs(n-1, False)
+    
+
+        # # TC: O(2n)
+        # # SC: O(2n)
+        # f = [[0] * 2 for _ in range(n+1)]
+        # f[0][1] = -inf
+
+        # for i, p in enumerate(prices):
+        #     # not hold
+        #     f[i+1][0] = max(f[i][0], f[i][1]+p)
+        #     # hold
+        #     f[i+1][1] = max(f[i][1], f[i-1][0]-p if i > 0 else -p)
+        
+        # return f[n][0]
+    
+        # TC: O(n)
+        # SC: O(1)
+        n = len(prices)
+
+        # f0: freeze, f1: not hold, f2: hold
+        f0 = f1 = 0
+        f2 = -inf
+
+        for i, p in enumerate(prices):
+            f0, f1, f2 = f1, max(f1, f2+p), max(f2, f0-p)
+
+        return f1
 # @lc code=end
 
