455a787b0a79304e8af138847b2b90aa7a23920a
yoyitowang <yoyowang0720@gmail.com>
2025-03-01 02:53:05 +0800

    add dp

--- problems/python/674.longest-continuous-increasing-subsequence.py
+++ problems/python/674.longest-continuous-increasing-subsequence.py
@@ -7,17 +7,29 @@
 # @lc code=start
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
+        # # TC: O(n)
+        # # SC: O(1)
+        # n = len(nums)
+        # ans = 0
+        # l = 0
+        # for i in range(n):
+        #     # when not incremental, update the left index
+        #     if i > 0 and nums[i-1] >= nums[i]:
+        #         l = i
+        #     ans = max(ans, i-l+1)
+        
+        # return ans
+
+        # dp
         # TC: O(n)
         # SC: O(1)
         n = len(nums)
-        ans = 0
-        l = 0
-        for i in range(n):
-            # when not incremental, update the left index
-            if i > 0 and nums[i-1] >= nums[i]:
-                l = i
-            ans = max(ans, i-l+1)
-        
-        return ans
+        f = [1] * n
+
+        for i in range(1, n):
+            if nums[i] > nums[i-1]:
+                f[i] = f[i-1] + 1
+
+        return max(f)
 # @lc code=end
 
