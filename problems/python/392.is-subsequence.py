8b7f2eac1810da01375693fad0d16c60551cbbf3
yoyitowang <yoyowang0720@gmail.com>
2025-03-02 01:34:16 +0800

    update 2-dim dp

--- problems/python/392.is-subsequence.py
+++ problems/python/392.is-subsequence.py
@@ -21,21 +21,37 @@ class Solution:
 
         # return left == n1
     
-        # dfs
+        # # dfs
+        # # TC: O(n2)
+        # # SC: O(n2)
+        # n1, n2 = len(s), len(t)
+        
+        # @cache
+        # def dfs(i, j):
+        #     if i < 0:
+        #         return True
+        #     if j < 0:
+        #         return False
+        #     if s[i] == t[j]:
+        #         return dfs(i-1, j-1)
+        #     return dfs(i, j-1)
+
+        # return dfs(n1-1, n2-1)
+
+        # 2-dim DP
         # TC: O(n2)
         # SC: O(n2)
         n1, n2 = len(s), len(t)
+        f = [[False] * (n2+1) for _ in range(n1+1)]
+        for i in range(n2):
+            f[0][i] = True
         
-        @cache
-        def dfs(i, j):
-            if i < 0:
-                return True
-            if j < 0:
-                return False
-            if s[i] == t[j]:
-                return dfs(i-1, j-1)
-            return dfs(i, j-1)
-
-        return dfs(n1-1, n2-1)
+        for i, x in enumerate(s):
+            for j, y in enumerate(t):
+                if x == y:
+                    f[i+1][j+1] = f[i][j]
+                else:
+                    f[i+1][j+1] = f[i+1][j]
+        return f[n1][n2]
 # @lc code=end
 
