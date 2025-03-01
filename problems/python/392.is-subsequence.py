f89f886b7ef206e09ce6988b90eac032e9165b29
yoyitowang <yoyowang0720@gmail.com>
2025-03-02 01:30:49 +0800

    add dfs

--- problems/python/392.is-subsequence.py
+++ problems/python/392.is-subsequence.py
@@ -7,18 +7,35 @@
 # @lc code=start
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # TC: O(n1|n2)
-        # SC: O(1)
+        # # TC: O(n1|n2)
+        # # SC: O(1)
+        # n1, n2 = len(s), len(t)
+        # left = right = 0
+
+        # while left < n1 and right < n2:
+        #     if s[left] == t[right]:
+        #         left += 1
+        #         right += 1
+        #     else:
+        #         right += 1
+
+        # return left == n1
+    
+        # dfs
+        # TC: O(n2)
+        # SC: O(n2)
         n1, n2 = len(s), len(t)
-        left = right = 0
+        
+        @cache
+        def dfs(i, j):
+            if i < 0:
+                return True
+            if j < 0:
+                return False
+            if s[i] == t[j]:
+                return dfs(i-1, j-1)
+            return dfs(i, j-1)
 
-        while left < n1 and right < n2:
-            if s[left] == t[right]:
-                left += 1
-                right += 1
-            else:
-                right += 1
-
-        return left == n1
+        return dfs(n1-1, n2-1)
 # @lc code=end
 
