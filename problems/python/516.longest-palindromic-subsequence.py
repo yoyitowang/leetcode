ee263ea08265e820b01ed214205a70e391c1e589
yoyitowang <yoyowang0720@gmail.com>
2025-03-02 23:15:31 +0800

    add 516 w/ dfs

---
+++ problems/python/516.longest-palindromic-subsequence.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=516 lang=python3
+#
+# [516] Longest Palindromic Subsequence
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+
+        @cache
+        def dfs(i, j):
+            if i > j:
+                return 0
+            if i == j:
+                return 1
+            if s[i] == s[j]:
+                return dfs(i+1, j-1) + 2
+            return max(dfs(i+1, j), dfs(i, j-1))
+
+        return dfs(0, n-1)    
+# @lc code=end
+
