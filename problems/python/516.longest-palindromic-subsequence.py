290326d0839f49951d87ead58bd2529a752e7a3f
yoyitowang <yoyowang0720@gmail.com>
2025-03-02 23:37:20 +0800

    update 2-dim DP

--- problems/python/516.longest-palindromic-subsequence.py
+++ problems/python/516.longest-palindromic-subsequence.py
@@ -7,18 +7,35 @@
 # @lc code=start
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
+        # n = len(s)
+
+        # @cache
+        # def dfs(i, j):
+        #     if i > j:
+        #         return 0
+        #     if i == j:
+        #         return 1
+        #     if s[i] == s[j]:
+        #         return dfs(i+1, j-1) + 2
+        #     return max(dfs(i+1, j), dfs(i, j-1))
+        # return dfs(0, n-1)    
+
+        # TC: O(n^2)
+        # TC: O(n^2)
         n = len(s)
 
-        @cache
-        def dfs(i, j):
-            if i > j:
-                return 0
-            if i == j:
-                return 1
-            if s[i] == s[j]:
-                return dfs(i+1, j-1) + 2
-            return max(dfs(i+1, j), dfs(i, j-1))
+        f = [[0] * (n) for _ in range(n)]
+        for i in range(n):
+            f[i][i] = 1
 
-        return dfs(0, n-1)    
+        # f[i][j] = f[i+1][j-1] + 2
+        for i in range(n-1, -1, -1):
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    f[i][j] = f[i+1][j-1] + 2
+                else:
+                    f[i][j] = max(f[i+1][j], f[i][j-1])
+
+        return f[0][n-1]
 # @lc code=end
 
