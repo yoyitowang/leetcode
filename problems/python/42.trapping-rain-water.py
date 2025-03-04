e43e57b3630fb9912edabcc17cbc0c98551046ab
yoyitowang <yoyowang0720@gmail.com>
2025-03-04 23:03:32 +0800

    update monotone stack

--- problems/python/42.trapping-rain-water.py
+++ problems/python/42.trapping-rain-water.py
@@ -7,19 +7,35 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # # TC: O(n)
+        # # SC: O(1)
+        # ans = pre_max = sur_max = 0
+        # left, right = 0, len(height)-1
+        # while left < right:
+        #     pre_max = max(pre_max, height[left])
+        #     sur_max = max(sur_max, height[right])
+        #     if pre_max < sur_max:
+        #         ans += pre_max - height[left]
+        #         left += 1
+        #     else:
+        #         ans += sur_max - height[right]
+        #         right -= 1
+
+        # return ans
+    
         # TC: O(n)
-        # SC: O(1)
-        ans = pre_max = sur_max = 0
-        left, right = 0, len(height)-1
-        while left < right:
-            pre_max = max(pre_max, height[left])
-            sur_max = max(sur_max, height[right])
-            if pre_max < sur_max:
-                ans += pre_max - height[left]
-                left += 1
-            else:
-                ans += sur_max - height[right]
-                right -= 1
+        # SC: O(min(n, U))
+        st = []
+        ans = 0
+        for i, h in enumerate(height):
+            while st and height[st[-1]] < h:
+                bottom_h = height[st.pop()]
+                if len(st) == 0:
+                    break
+                left = st[-1]
+                dh = min(height[left], h) - bottom_h
+                ans += dh * (i - left - 1)
+            st.append(i)
 
         return ans
       
