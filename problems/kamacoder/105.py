d1b0e932aadb2da0d378726202ae34c519d6b82b
yoyitowang <yoyowang0720@gmail.com>
2025-03-11 21:50:39 +0800

    rework

--- problems/kamacoder/105.py
+++ problems/kamacoder/105.py
@@ -1,29 +1,3 @@
-# DFS
-n, k = map(int, input().split())
-
-from collections import defaultdict
-ht = defaultdict(list)
-for _ in range(k):
-    start, end = map(int, input().split())
-    ht[start].append(end)
-visit = [False for _ in range(n+1)]
-
-def dfs(i):
-    visit[i] = True
-    cnt = 1
-    for _next in ht[i]:
-        if not visit[_next]:
-            cnt += dfs(_next)
-
-    return cnt
-
-def main():
-    cnt = dfs(1)
-    print(1 if cnt==n else -1)
-
-main()
-
-# BFS
 n, k = map(int, input().split())
 
 from collections import defaultdict, deque
@@ -34,27 +8,37 @@ for _ in range(k):
     ht[start].append(end)
 visit = [False for _ in range(n+1)]
 
-def bfs(i):
-    cnt = 1
-    que = deque([i])
+def dfs(i):
     visit[i] = True
-    
+    num = 1
+
+    for _next in ht[i]:
+        if not visit[_next]:
+            num += dfs(_next)
+
+    return num
+
+def bfs(i):
+    visit[i] = True
+    num = 1
+    que = deque([i])
+
     while que:
-        node = que.popleft()
-        for _next in ht[node]:
+        _next = que.popleft()
+        for _next in ht[_next]:
             if visit[_next] == False:
-                cnt += 1
+                num += 1
                 visit[_next] = True
                 que.append(_next)
 
-    return cnt
+    return num
 
 def main():
-    if k == 0:
-        print(1 if n == 1 else -1)
-        return
 
-    cnt = bfs(1)
-    print(1 if cnt==n else -1)
+    # ans = dfs(1)
+    ans = bfs(1)
+    print(1 if ans == n else -1)
 
 main()
+    
+    
