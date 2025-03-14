b946ce08e0ff30a26b38277f33a67df4389de343
yoyitowang <yoyowang0720@gmail.com>
2025-03-14 23:58:31 +0800

    add 739

--- problems/python/739.daily-temperatures.py
+++ problems/python/739.daily-temperatures.py
@@ -7,15 +7,30 @@
 # @lc code=start
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # # TC: O(n)
+        # # SC: O(n)
+        # n = len(temperatures)
+        # ans = [0] * n
+        # st = []
+        # for i, t in enumerate(temperatures):
+        #     while st and temperatures[st[-1]] < t:
+        #         j = st.pop()
+        #         ans[j] = i-j
+        #     st.append(i)
+
+        # return ans
+    
         # TC: O(n)
-        # SC: O(n)
+        # SC: O(min(n, U))
         n = len(temperatures)
-        ans = [0] * n
+        ans = [0] * n 
         st = []
-        for i, t in enumerate(temperatures):
-            while st and temperatures[st[-1]] < t:
-                j = st.pop()
-                ans[j] = i-j
+        for i in range(n-1, -1, -1):
+            t = temperatures[i]
+            while st and temperatures[st[-1]] <= t:
+                st.pop()
+            if st:
+                ans[i] = st[-1] - i
             st.append(i)
 
         return ans
