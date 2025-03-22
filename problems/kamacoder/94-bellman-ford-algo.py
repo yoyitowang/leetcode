1415809dd2b972581eb88ce7048956d2bce5a512
yoyitowang <yoyowang0720@gmail.com>
2025-03-22 22:24:14 +0800

    fix error

--- problems/kamacoder/94-bellman-ford-algo.py
+++ problems/kamacoder/94-bellman-ford-algo.py
@@ -61,50 +61,56 @@ from collections import deque, defaultdict
 class SPFA:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = defaultdict(list)
         self.dist = [float('inf')] * vertices
-        self.count = 0
+        self.graph = defaultdict(list)
+        self.parent = [-1] * vertices
+        self.count = [0] * vertices
 
     def add_edge(self, u, v, w):
         self.graph[u].append((v, w))
 
+    def get_path(self, target):
+        path = []
+        cur = target
+        while cur != -1:
+            path.append(cur)
+            cur = self.parent[cur]
+            
+        return path[::-1]
+
     def find_shortest_costs(self, source):
-        visited = [False] * self.V
-        self.dist[source] = 0
         que = deque([source])
-        # visited[source] = True
+        in_que = [False] * self.V
+        self.dist[source] = 0
 
         while que:
             u = que.popleft()
-            visited[u] = False
-
-            # check every next path of vertex u
+            in_que[u] = False
+            
             for v, w in self.graph[u]:
-                # only when new cost < dist[u] -> update
                 if self.dist[u] + w < self.dist[v]:
                     self.dist[v] = self.dist[u] + w
-
-                # check is already in queue            
-                if not visited[v]:
-                    self.count += 1
-                    # check negative cycle
-                    if self.count >= self.V:
-                        return False # negaive cycle
-                    que.append(v)
-                    visited[v] = True
+                    self.parent[v] = u
+                
+                    if not in_que[v]:
+                        self.count[v] += 1
+                        if self.count[v] >= self.V:
+                            return False
+                        que.append(v)
+                        in_que[v] = True
         return True
 
 if __name__ == "__main__":
-    n, m = map(int, input().strip().split())
-    edges = []
+    n, m = map(int, input().split())
     spfa = SPFA(n+1)
 
     for _ in range(m):
-        src, dest, weight = map(int, input().strip().split())
-        spfa.add_edge(src, dest, weight)
+        u, v, w = map(int, input().split())
+        spfa.add_edge(u, v, w)
 
-    if not spfa.find_shortest_costs(1) or spfa.dist[n] == float('inf'):
-        print("unconnected")
-    else:
-        print(spfa.dist[n])
-
+    if spfa.find_shortest_costs(1):
+        if spfa.dist[n] != float('inf'):
+            print(spfa.dist[n])
+            print(spfa.get_path(n))
+            exit()
+    print("unconnected")
