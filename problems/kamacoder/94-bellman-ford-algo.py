082e0769f64519ce1464aa51eec23b3c7c78d037
yoyitowang <yoyowang0720@gmail.com>
2025-03-22 15:46:19 +0800

    add bellman ford path function

--- problems/kamacoder/94-bellman-ford-algo.py
+++ problems/kamacoder/94-bellman-ford-algo.py
@@ -1,46 +1,59 @@
-class BellmanFord:
+from collections import deque, defaultdict
+
+class bellman_ford:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = []
         self.dist = [float('inf')] * vertices
-        
+        self.graph = []
+        self.parent = [-1] * vertices
+        self.count = 0
+
     def add_edge(self, u, v, w):
         self.graph.append((u, v, w))
 
+    def get_path(self, target):
+        path = []
+        cur = target
+        while cur != -1:
+            path.append(cur)
+            cur = self.parent[cur]
+            
+        return path[::-1]
+
     def find_shortest_costs(self, source):
+        visited = [False] * self.V
         self.dist[source] = 0
-        
-        for i in range(self.V-1):
+
+        for _ in range(self.V-1):
             updated = False
             for u, v, w in self.graph:
                 if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
                     self.dist[v] = self.dist[u] + w
+                    self.parent[v] = u
                     updated = True
-            
             if not updated:
                 break
 
         for u, v, w in self.graph:
-            if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
-                return False  # 存在負環
+                if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
+                    return False
 
         return True
-        
 
 if __name__ == "__main__":
-    n, m = map(int, input().strip().split())
-    edges = []
-    bell = BellmanFord(n+1)
+    n, m = map(int, input().split())
+    bellman = bellman_ford(n+1)
 
     for _ in range(m):
-        src, dest, weight = map(int, input().strip().split())
-        bell.add_edge(src, dest, weight)
+        u, v, w = map(int, input().split())
+        bellman.add_edge(u, v, w)
 
-    if not bell.find_shortest_costs(1) or bell.dist[n] == float('inf'):
-        print("unconnected")
+    if bellman.find_shortest_costs(1) and bellman.dist[n] != float('inf'):
+        print(bellman.dist[n])
+        print(bellman.get_path(n))
     else:
-        print(bell.dist[n])
-        
+        print("unconnected")
+
 ---
 # SPFA
 from collections import deque, defaultdict
