a51c4875e533c0d80cad7ef0f719478af90ebcdb
yoyitowang <yoyowang0720@gmail.com>
2025-03-24 23:29:08 +0800

    udpated

--- problems/kamacoder/95-bellman-ford-algo.py
+++ problems/kamacoder/95-bellman-ford-algo.py
@@ -40,3 +40,56 @@ if __name__ == '__main__':
             print(ans)
         else: # can't arrive final vertex
             print("unconnected")
+
+---
+# SPFA
+from collections import defaultdict, deque
+
+class SPFA:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.dist = [float('inf')] * vertices
+        self.graph = defaultdict(list)
+        self.count = [0] * vertices
+
+    def add_edge(self, u, v, w):
+        self.graph[u].append((v, w))
+
+    def find_shortest_path(self, source):
+        in_queue = [False] * self.V
+        que = deque([source])
+        # init
+        self.dist[source] = 0
+        in_queue[source] = True
+        
+        while que:
+            u = que.popleft()
+            in_queue[u] = False
+
+            for v, w in self.graph[u]:
+                if self.dist[u] + w < self.dist[v]:
+                    self.dist[v] = self.dist[u] + w
+                
+                    if not in_queue[v]:
+                        self.count[v] += 1
+                        if self.count[v] >= self.V:
+                            print("circle")
+                            exit()
+                        que.append(v)
+                        in_queue[v] = True
+        
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    spfa = SPFA(n+1)
+
+    for _ in range(m):
+        u, v, w = map(int, input().split())
+        spfa.add_edge(u, v, w)
+
+    spfa.find_shortest_path(1)
+    ans = spfa.dist[n]
+    if ans != float('inf'):
+        print(ans)
+    else: # can't arrive final vertex
+        print("unconnected")
+        
