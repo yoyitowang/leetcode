a9c05843fa5d8adab820e82edad02c95af0eb183
yoyitowang <yoyowang0720@gmail.com>
2025-03-25 23:25:12 +0800

    add SLA in SPFA

--- problems/kamacoder/94-bellman-ford-algo.py
+++ problems/kamacoder/94-bellman-ford-algo.py
@@ -3,40 +3,40 @@ from collections import deque, defaultdict
 class bellman_ford:
     def __init__(self, vertices):
         self.V = vertices
-        self.dist = [float('inf')] * vertices
         self.graph = []
+        self.min_dist = [float('inf')] * self.V
         self.parent = [-1] * vertices
-        self.count = 0
 
     def add_edge(self, u, v, w):
-        self.graph.append((u, v, w))
+        self.graph.append([u, v, w])
 
     def get_path(self, target):
-        path = []
+        res = []
         cur = target
         while cur != -1:
-            path.append(cur)
+            res.append(cur)
             cur = self.parent[cur]
-            
-        return path[::-1]
+        if res[-1] != 1:
+            return []
+        return res[::-1]
 
     def find_shortest_costs(self, source):
-        visited = [False] * self.V
-        self.dist[source] = 0
+        self.min_dist[source] = 0
 
         for _ in range(self.V-1):
             updated = False
             for u, v, w in self.graph:
-                if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
-                    self.dist[v] = self.dist[u] + w
+                if self.min_dist[u] != float('inf') and self.min_dist[u] + w < self.min_dist[v]:
+                    self.min_dist[v] = self.min_dist[u] + w
                     self.parent[v] = u
                     updated = True
+        
             if not updated:
                 break
-
+        
         for u, v, w in self.graph:
-                if self.dist[u] != float('inf') and self.dist[u] + w < self.dist[v]:
-                    return False
+            if self.min_dist[u] != float('inf') and self.min_dist[u] + w < self.min_dist[v]:
+                return False
 
         return True
 
@@ -48,8 +48,8 @@ if __name__ == "__main__":
         u, v, w = map(int, input().split())
         bellman.add_edge(u, v, w)
 
-    if bellman.find_shortest_costs(1) and bellman.dist[n] != float('inf'):
-        print(bellman.dist[n])
+    if bellman.find_shortest_costs(1) and bellman.min_dist[n] != float('inf'):
+        print(bellman.min_dist[n])
         print(bellman.get_path(n))
     else:
         print("unconnected")
@@ -61,43 +61,48 @@ from collections import deque, defaultdict
 class SPFA:
     def __init__(self, vertices):
         self.V = vertices
-        self.dist = [float('inf')] * vertices
         self.graph = defaultdict(list)
+        self.dist = [float('inf')] * vertices
         self.parent = [-1] * vertices
-        self.count = [0] * vertices
 
     def add_edge(self, u, v, w):
         self.graph[u].append((v, w))
 
     def get_path(self, target):
-        path = []
+        ans = []
         cur = target
         while cur != -1:
-            path.append(cur)
+            ans.append(cur)
             cur = self.parent[cur]
-            
-        return path[::-1]
+
+        if ans[-1] != 1:
+            return []
+        return ans[::-1]
 
     def find_shortest_costs(self, source):
-        que = deque([source])
-        in_que = [False] * self.V
         self.dist[source] = 0
+        count = [0] * self.V
+        visited = [False] * self.V
+        que = deque([source])
 
         while que:
             u = que.popleft()
-            in_que[u] = False
+            visited[u] = False
             
             for v, w in self.graph[u]:
                 if self.dist[u] + w < self.dist[v]:
                     self.dist[v] = self.dist[u] + w
                     self.parent[v] = u
-                
-                    if not in_que[v]:
-                        self.count[v] += 1
-                        if self.count[v] >= self.V:
+
+                    if not visited[v]:
+                        count[v] += 1
+                        if count[v] >= self.V:
                             return False
-                        que.append(v)
-                        in_que[v] = True
+                        if que and self.dist[v] < self.dist[que[0]]:
+                            que.appendleft(v)
+                        else:
+                            que.append(v)
+                        visited[v] = True
         return True
 
 if __name__ == "__main__":
@@ -111,6 +116,6 @@ if __name__ == "__main__":
     if spfa.find_shortest_costs(1):
         if spfa.dist[n] != float('inf'):
             print(spfa.dist[n])
-            print(spfa.get_path(n))
+            # print(spfa.get_path(n))
             exit()
     print("unconnected")
