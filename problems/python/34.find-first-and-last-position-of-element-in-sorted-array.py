707d83665f70de249b29d6c7cd36c5b8c9452904
yoyitowang <yoyowang0720@gmail.com>
2025-04-06 15:30:16 +0800

    update open/close functions

--- problems/python/34.find-first-and-last-position-of-element-in-sorted-array.py
+++ problems/python/34.find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,29 +9,51 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         # TC: O(log n)
         # SC: O(1)
-        def findLeft(nums, target):
-            left, right = 0, len(nums)-1
+
+        # -- [left, right]
+        def lower_bound(nums, target):
+            n = len(nums)
+            left, right = 0, n-1
+
             while left <= right:
-                mid = (left+right)//2
+                mid = (left+right) // 2
                 if nums[mid] < target:
                     left = mid + 1
                 else:
                     right = mid - 1
-            if left < len(nums) and nums[left] == target:
-                return left
-            return -1
-        def findRight(nums, target):
-            left, right = 0, len(nums)-1
-            while left <= right:
-                mid = (left+right)//2
-                if nums[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            if right >= 0 and nums[right] == target:
-                return right
-            return -1
+            return left
             
-        return [findLeft(nums, target), findRight(nums, target)]
+        # -- [left, right)
+        def lower_bound2(nums, target):
+            n = len(nums)
+            left, right = 0, n
+
+            while left < right:
+                mid = (left+right) // 2
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
+            
+        # -- (left, right)
+        def lower_bound3(nums, target):
+            n = len(nums)
+            left, right = -1, n
+
+            while left + 1 < right:
+                mid = (left+right) // 2
+                if nums[mid] < target:
+                    left = mid
+                else:
+                    right = mid
+            return right
+        
+        first = lower_bound3(nums, target)
+        if first == len(nums) or nums[first] != target:
+            return [-1, -1]
+        last = lower_bound3(nums, target+1) - 1
+        return [first, last]
+
 # # @lc code=end
 
